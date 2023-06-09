c6ec32c1436eba7faf974d5e82cccdd2421b5d09
uros <uros.mrkobrada99@gmail.com>
2023-06-09 22:28:30 +0200

    added flags to sd

--- tsnuxt.sh
+++ tsnuxt.sh
@@ -4,8 +4,16 @@ startjs="var ts ="
 temppImport='const tempp = require("../lib/temp");'
 resultsStart="withoutExpandos : filteredDeclarations;"
 nuxtComp="tempp.nuxtComp(results, node);"
+fromNameFn="(function createDefinitionInfoFromName\(.*\).*\n.*getSourceFile.*;)"
+fromNameFn2="(function createDefinitionInfoFromName\(.*\).*\n.*return\{fileName:);"
+changedFile="sourceFile tfn = sourceFile.tempFileName;sourceFile fileName = tfn ? tfn : sourceFile.fileName; "
 
 cp -r "$tsdir/typescript.js" "$tsdir/original-typescript.js"
-mv temp.js "$tsdir"
+cp -r temp.js "$tsdir"
 sd "$startjs" "$temppImport\n$startjs" "$tsdir/typescript.js"
 sd "$resultsStart" "$resultsStart\n  $nuxtComp\n" "$tsdir/typescript.js"
+sd "$fromNameFn" '$1
+  let tfn = sourceFile.tempFileName;
+  let fileName = tfn ? tfn : sourceFile.fileName;' "$tsdir/typescript.js"
+
+sd -f ms "(function createDefinitionInfoFromName\(.*?fileName:).*?,(.*?;.*?\})" '$1 fileName, $2' "$tsdir/typescript.js"
